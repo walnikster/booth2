a64a746025a94faa51763f215723aa430d80da55
niki <niki@theliferocks.com>
2021-12-17 19:58:33 +0100

    initial java proj

---
+++ src/main/java/com/uleos/boot/booth2/Booth2Application.java
@@ -0,0 +1,13 @@
+package com.uleos.boot.booth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Booth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Booth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uleos/boot/booth2/Booth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uleos.boot.booth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Booth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
