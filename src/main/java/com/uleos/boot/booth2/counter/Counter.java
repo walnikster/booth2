d74da947d70d36279905302ea643071eba08542f
niki <niki@theliferocks.com>
2023-01-24 18:29:42 +0100

    logging, java imports

--- src/main/java/com/uleos/boot/booth2/counter/Counter.java
+++ src/main/java/com/uleos/boot/booth2/counter/Counter.java
@@ -1,12 +1,12 @@
 package com.uleos.boot.booth2.counter;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Getter
